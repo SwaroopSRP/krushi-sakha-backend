72ac121cfe60be647a442437ed779282286bdecb
SwaroopSRP <srp31.swaroop@gmail.com>
2025-09-21 01:58:04 +0530

    Impl Azure Translator API

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import os from "node:os";
 import app from "./app.js";
 
 dotenv.config({
@@ -7,6 +8,19 @@ dotenv.config({
 
 const port = process.env.PORT || 6900;
 
-app.listen(port, () => {
+function getLocalIP() {
+    const nets = os.networkInterfaces();
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            if (net.family === "IPv4" && !net.internal) {
+                return net.address;
+            }
+        }
+    }
+    return null;
+}
+
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server up and running on http://localhost:${port}`);
+    console.log(`Local Address: http://${getLocalIP()}:${port}`);
 });
---
+++ src/services/translator.services.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+
+let key = process.env.AZURE_TRANSLATOR_KEY;
+let location = process.env.AZURE_TRANSLATOR_REGION;
+let endpoint = "https://api.cognitive.microsofttranslator.com";
+
+async function translateText(text, mode = 0) {
+    try {
+        const response = await axios({
+            baseURL: endpoint,
+            url: "/translate",
+            method: "post",
+            headers: {
+                "Ocp-Apim-Subscription-Key": key,
+                "Ocp-Apim-Subscription-Region": location,
+                "Content-type": "application/json",
+                "X-ClientTraceId": uuidv4().toString()
+            },
+            params: {
+                "api-version": "3.0",
+                from: mode ? "en" : "ml",
+                to: mode ? "ml" : "en"
+            },
+            data: [
+                {
+                    text: text
+                }
+            ],
+            responseType: "json"
+        });
+        const translation = response.data[0].translations[0].text;
+
+        return translation;
+    } catch (err) {
+        console.error("Translation error:", err.response?.data || err.message);
+        throw err;
+    }
+}
